c7cb8503882338b2a7b57cf8b6a8ae5aa7f237e2
Filippo Cerchi <filippocerchi@MacBook-Pro-di-Filippo.local>
2023-02-03 19:58:52 +0100

    Prova Combattimento

---
+++ scenes/CombatScene/Personaggio.gd
@@ -0,0 +1,15 @@
+extends Sprite
+
+# Statistiche
+var vita:int = 100
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	$ProgressBar.max_value = vita
+	$ProgressBar.value = vita
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
