768599752b45fcdb273b5b75d692efd7dd79b51a
Fil <pulce2011@github.com>
2023-02-03 20:36:00 +0100

    Combattimento tasto C

--- scenes/CombatScene/Combattimento.gd
+++ scenes/CombatScene/Combattimento.gd
@@ -5,7 +5,13 @@ extends Node2D
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass
+	
+func _input(event):
+	if(event.is_action("combatti")):
+		$Alleato.combat("Alleato")
+		$Nemico.vita -= 1
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _process(delta):
+	pass
+	
--- scenes/CombatScene/Personaggio.gd
+++ scenes/CombatScene/Personaggio.gd
@@ -3,13 +3,14 @@ extends Sprite
 # Statistiche
 var vita:int = 100
 
-
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	$ProgressBar.max_value = vita
 	$ProgressBar.value = vita
 
+func combat(string):
+	print(string)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	$ProgressBar.value = vita
