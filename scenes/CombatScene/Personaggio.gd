a05f36a1973e9912f7c1c3f8933d151a8fc3c0d6
Fil <pulce2011@github.com>
2023-02-04 10:28:12 +0100

    Scena combattimento centrata

--- scenes/CombatScene/Personaggio.gd
+++ scenes/CombatScene/Personaggio.gd
@@ -8,10 +8,9 @@ func combat(string):
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	$Personaggio/ProgressBar.max_value = vita
-	$Personaggio/ProgressBar.value = vita
+	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
-	$Personaggio/ProgressBar.value = vita
+	pass
 
